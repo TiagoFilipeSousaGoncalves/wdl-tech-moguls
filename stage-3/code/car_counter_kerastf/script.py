d3797c35de6e17203d708787280fbc11865f65e4
Tiago Gonçalves <tiago.galves@hotmail.com>
2021-05-08 00:42:08 +0100

    Added car counter in Keras, TF and CVLib

---
+++ stage-3/code/car_counter_kerastf/script.py
@@ -0,0 +1,17 @@
+# Imports
+import cv2
+import matplotlib.pyplot as plt
+import cvlib as cv
+from cvlib.object_detection import draw_bbox
+
+# Read image
+im = cv2.imread('dset_img_2.jpg')
+
+# Count cars
+bbox, label, conf = cv.detect_common_objects(im)
+output_image = draw_bbox(im, bbox, label, conf)
+
+# Show
+plt.imshow(output_image)
+plt.show()
+print('Number of cars in the image is '+ str(label.count('car')))
